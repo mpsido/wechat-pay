e04c9f578977832d72a821dc67623b96d4fe7993
mpsido <1979371837@qq.com>
2020-10-13 11:48:14 +0800

    var -> const

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(3001, function () {
-   var host = server.address().address
-   var port = server.address().port
+const server = app.listen(3001, function () {
+   const host = server.address().address
+   const port = server.address().port
    
    console.log("Example app listening at http://%s:%s", host, port)
 })
