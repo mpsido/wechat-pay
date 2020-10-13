a48788f465f0c27762806f9aff581c06f87bf913
mpsido <1979371837@qq.com>
2020-10-13 11:59:45 +0800

    Starting to use wechat sandbox

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const tenpay = require('tenpay');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.text({type: '*/xml'}));
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
+
+const config = {
+  appid: 'wx440c2467b90817aa',
+  mchid: '1500059462',
+  partnerKey: 'uMQKLbXzRqss0xPRtPLNdaskldoi2ehu',
+  // pfx: require('fs').readFileSync('证书文件路径'),
+  // notify_url: '支付回调网址',
+  // spbill_create_ip: 'IP地址'
+};
+
+const api = new tenpay(config);
+
+app.post('/tenpay', (req, res) => {
+    console.log('calling tenpay');
+    tenpay.sandbox(config).then(sandboxAPI => {
+        console.log('sandboxAPI', sandboxAPI);
+        let info = req.weixin;
+      // 业务逻辑...
+      // 回复消息(参数为空回复成功, 传值则为错误消息)
+        res.send('错误消息' || '');
+    });
+ 
+});
 
 const server = app.listen(3001, function () {
    const host = server.address().address
